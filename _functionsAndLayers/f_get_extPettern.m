991dfd2c21a78f8be6682f75fa2c413f4f114b58
Dushan N. Wadduwage <15632687+dushanw@users.noreply.github.com>
2020-09-09 18:41:59 -0400

    added experimental patterns(waveguide and dmd) and modified codes accordingly.

--- _functionsAndLayers/f_get_extPettern.m
+++ _functionsAndLayers/f_get_extPettern.m
@@ -2,8 +2,12 @@
 function E = f_get_extPettern(pram)
 
   switch pram.pattern_typ
-    case 'rnd'
+    case 'dmd_sim_rnd'
       E = rand([pram.Ny pram.Nx pram.Nt])-.5; % for DMDs
+    case 'wgd_sim'
+      load wgd_simMMI.mat
+      E = E0(size(E0,1)/2-pram.Ny/2+1:end,size(E0,1)/2-pram.Nx/2+1:end,:);
+      E = E(1:pram.Ny,1:pram.Nx,1:pram.Nt);
   end
 
 end
