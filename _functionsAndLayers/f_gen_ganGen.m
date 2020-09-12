0eafc3f03f80dcdf54b6f97af5f86e8a7b33eb17
Dushan N. Wadduwage <15632687+dushanw@users.noreply.github.com>
2020-09-11 21:54:04 -0400

    added a generator for gans.

---
+++ _functionsAndLayers/f_gen_ganGen.m
@@ -0,0 +1,30 @@
+% 20200911 by Dushan N. Wadduwage
+% Generator network for GAN (same network in the example: https://www.mathworks.com/help/deeplearning/ug/train-generative-adversarial-network.html)
+
+function dlnetGenerator = f_gen_ganGen(pram)
+
+  numFilters      = pram.numFilters;
+  numLatentInputs = pram.Ncompressed_gen;
+
+  filterSize = 5;
+  projectionSize = [4 4 512];
+
+  layersGenerator = [
+      imageInputLayer([1 1 numLatentInputs],'Normalization','none','Name','in')
+      projectAndReshapeLayer(projectionSize,numLatentInputs,'proj');
+      transposedConv2dLayer(filterSize,4*numFilters,'Name','tconv1')
+      batchNormalizationLayer('Name','bnorm1')
+      reluLayer('Name','relu1')
+      transposedConv2dLayer(filterSize,2*numFilters,'Stride',2,'Cropping','same','Name','tconv2')
+      batchNormalizationLayer('Name','bnorm2')
+      reluLayer('Name','relu2')
+      transposedConv2dLayer(filterSize,numFilters,'Stride',2,'Cropping','same','Name','tconv3')
+      batchNormalizationLayer('Name','bnorm3')
+      reluLayer('Name','relu3')
+      transposedConv2dLayer(filterSize,1,'Stride',2,'Cropping','same','Name','tconv4')
+      tanhLayer('Name','tanh')];
+
+  lgraphGenerator = layerGraph(layersGenerator);
+  dlnetGenerator  = dlnetwork(lgraphGenerator);
+
+end
