94667eda6faafb2b705a20caa73ca5a95d1f0850
Dushan N. Wadduwage <15632687+dushanw@users.noreply.github.com>
2020-09-13 08:58:16 -0700

    Modified f_train_gan to display the training scores while training, in addition to displaying the validation results.

--- _functionsAndLayers/_networkTrainers/f_train_gan.m
+++ _functionsAndLayers/_networkTrainers/f_train_gan.m
@@ -13,7 +13,6 @@ function [nets info] = f_train_gan(XTrain,XVal,Models,opt)
   dlnetDiscriminator  = Models.dlnetDiscriminator;
   flipFactor          = opt.flipFactor;
   
-  
   dlXVal              = dlarray(XVal  ,'SSCB');
   ZVal                = randn([dlnetGenerator.Layers(1).InputSize size(XVal,4)],'single');
   dlZVal              = dlarray(ZVal,'SSCB');
@@ -23,22 +22,33 @@ function [nets info] = f_train_gan(XTrain,XVal,Models,opt)
     dlZVal  = gpuArray(dlZVal);        
   end
 
-  figure
+  f = figure;
+  f.Position(3) = 2*f.Position(3);
+  imageAxes = subplot(1,2,1);
+  scoreAxes = subplot(1,2,2);
+  lineScoreGenerator = animatedline(scoreAxes,'Color',[0 0.447 0.741]);
+  lineScoreDiscriminator = animatedline(scoreAxes, 'Color', [0.85 0.325 0.098]);
+  legend('Generator','Discriminator');
+  ylim([0 1])
+  xlabel("Iteration")
+  ylabel("Score")
+  grid on
+  
   iteration       = 0;
   start           = tic;
   numTrainImages  = size(XTrain,4);
-
   for i = 1:opt.numEpochs
     XTrain = XTrain(:,:,:,randperm(numTrainImages));
 
     for ii=1:opt.miniBatchSize:numTrainImages-opt.miniBatchSize 
       iteration   = iteration + 1;
       
-      X           = XTrain(:,:,:,ii:ii+opt.miniBatchSize-1);
+      X = XTrain(:,:,:,ii:ii+opt.miniBatchSize-1);      
+      X = rescale(X,-1,1,'InputMin',0,'InputMax',2);                       % Rescale the images in the range [-1 1].
+      Z = randn([dlnetGenerator.Layers(1).InputSize size(X,4)],'single');
+      
       dlX         = dlarray(X, 'SSCB');
-      Z           = randn([dlnetGenerator.Layers(1).InputSize size(X,4)],'single');
       dlZ         = dlarray(Z, 'SSCB');
-
       if (opt.executionEnvironment == "auto" && canUseGPU) || opt.executionEnvironment == "gpu"
           dlX = gpuArray(dlX);
           dlZ = gpuArray(dlZ);
@@ -66,19 +76,31 @@ function [nets info] = f_train_gan(XTrain,XVal,Models,opt)
       if mod(iteration,20) == 0 || iteration == 1                        
         dlXGeneratedValidation  = predict(dlnetGenerator,dlZVal);
 
-        I   = imtile(cat(2,extractdata(dlXVal),extractdata(dlXGeneratedValidation),zeros(size(dlXVal,1),10,1,size(dlXVal,4))));
-        I   = rescale(I);
-        imagesc(I);axis image
+        I = imtile(cat(2,extractdata(dlXVal),extractdata(dlXGeneratedValidation),zeros(size(dlXVal,1),10,1,size(dlXVal,4))));
+        I = rescale(I);
+        
+        subplot(1,2,1);
+        imagesc(imageAxes,I)
+        xticklabels([]);
+        yticklabels([]);
+        title("Generated Images");
+      end
+      % Update the scores plot
+      subplot(1,2,2)
+      addpoints(lineScoreGenerator,iteration,...
+        double(gather(extractdata(losses_itr(3)))));
 
-        % Update the title with training progress information.
-        D = duration(0,0,toc(start),'Format','hh:mm:ss');
-        title(...
-          "Epoch: " + i + ", " + ...
-          "Iteration: " + iteration + ", " + ...
-          "Elapsed: " + string(D))
+      addpoints(lineScoreDiscriminator,iteration,...
+        double(gather(extractdata(losses_itr(4)))));
 
-        drawnow
-      end            
+      % Update the title with training progress information.
+      D = duration(0,0,toc(start),'Format','hh:mm:ss');
+      title(...
+        "Epoch: " + i + ", " + ...
+        "Iteration: " + iteration + ", " + ...
+        "Elapsed: " + string(D))
+        
+      drawnow
     end
   end
 
@@ -121,24 +143,22 @@ function [gradientsGenerator, gradientsDiscriminator, stateGenerator, allLosses]
   probReal(:,:,:,idx) = 1-probReal(:,:,:,idx);
 
   % Calculate the GAN loss.
-  [lossGenerator, lossDiscriminator, allLosses] = ganLoss(probReal,probGenerated);
+  [lossGenerator, lossDiscriminator] = ganLoss(probReal,probGenerated);
 
   % For each network, calculate the gradients with respect to the loss.
   gradientsGenerator = dlgradient(lossGenerator, dlnetGenerator.Learnables,'RetainData',true);
   gradientsDiscriminator = dlgradient(lossDiscriminator, dlnetDiscriminator.Learnables);
 
+  allLosses = [lossGenerator lossDiscriminator scoreGenerator scoreDiscriminator];
 end
 
 
-function [lossGenerator, lossDiscriminator, allLosses] = ganLoss(probReal,probGenerated)
-
+function [lossGenerator, lossDiscriminator] = ganLoss(probReal,probGenerated)
   % Calculate the loss for the discriminator network.
   lossDiscriminator =  -mean(log(probReal)) -mean(log(1-probGenerated));
 
   % Calculate the loss for the generator network.
   lossGenerator = -mean(log(probGenerated));
-
-  allLosses = [lossGenerator lossDiscriminator];
 end
 
 
