9eb1372517b4ee6124c25bbc89a63b07df985ca3
Dushan N. Wadduwage <15632687+dushanw@users.noreply.github.com>
2020-09-17 06:04:36 -0700

    Introduced the project and reshape layer to std gen (replacing tconv1). Now the featre space is fully cnnected to the secondlayer first. so the pixel postions in the image matters.

--- _functionsAndLayers/f_gen_stdGen.m
+++ _functionsAndLayers/f_gen_stdGen.m
@@ -1,13 +1,20 @@
 
-function dlnetGenerator = f_gen_stdGen(Nx,Nc,Ncompressed,numFilters)
+function dlnetGenerator = f_gen_stdGen(pram)
     
-    filterSize      = [4 4];
+    Nx              = pram.Nx;
+    Nc              = pram.Nc;
+    Ncompressed     = pram.Ncompressed_gen;
+    numFilters      = pram.numFilters;
+    
+    projectionSize  = [4 4 512];
+    filterSize      = 4;
     N_middleLayers  = log2(Nx)-2;    
     
     layersGenerator = [
         imageInputLayer([1 1 Ncompressed],'Normalization','none','Name','in');
-        transposedConv2dLayer(filterSize,2.^(N_middleLayers-1)*numFilters,'Name','tconv1')
-        batchNormalizationLayer('Name','bn1')
+        projectAndReshapeLayer(projectionSize,Ncompressed,'proj');
+        % transposedConv2dLayer(filterSize,2.^(N_middleLayers-1)*numFilters,'Name','tconv1')
+        % batchNormalizationLayer('Name','bn1')
         reluLayer('Name','relu1')];
 
     for i=2:N_middleLayers    
