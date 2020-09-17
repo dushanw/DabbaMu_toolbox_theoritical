833ebeadc7427ebe1fe9d6b83d883e6d62d9c468
Dushan N. Wadduwage <15632687+dushanw@users.noreply.github.com>
2020-09-17 05:41:14 -0700

    Intoducd GNA training lines to the main.m code. Minor changes to pram_init.

--- main.m
+++ main.m
@@ -11,14 +11,33 @@ addpath('./_ExtPatternsets/')
 pram            = pram_init(); 
 X               = f_get_dataset(pram);
 
-%% initiate and train generator
-lgraph_autoEnc  = f_gen_stdAutoEnc(pram);
-trOptions       = f_set_training_options(pram,X.Val,X.Val);
-net_autoEnc     = trainNetwork(X.Train,X.Train,lgraph_autoEnc,trOptions);
-dlnet_gen       = f_get_gen(pram,net_autoEnc);
-
+%% initiate and train generator using AUTOENCODER
+% lgraph_autoEnc  = f_gen_stdAutoEnc(pram);
+% pram.excEnv     = 'multi-gpu'; 
+% trOptions       = f_set_training_options(pram,X.Val,X.Val);
+% net_autoEnc     = trainNetwork(X.Train,X.Train,lgraph_autoEnc,trOptions);
+% dlnet_gen       = f_get_gen(pram,net_autoEnc);
+% 
+% XhatTest_ae     = predict(net_autoEnc,X.Val);
+% imagesc(imtile([X.Val XhatTest_ae]));axis image;colorbar
+% 
 % XhatTest        = predict(dlnet_gen,dlarray(10*rand(1,1,pram.Ncompressed_gen,100),'SSCB'));
-% imagesc(imtile([XhatTest.extractdata]))
+% imagesc(imtile([XhatTest.extractdata]));axis image;colorbar
+
+%% initiate and train generator using GAN
+Models.dlnetGenerator      = f_gen_stdGen(pram);
+%Models.dlnetGenerator      = f_gen_ganGen(pram);
+Models.dlnetDiscriminator  = f_gen_stdDisc(pram);
+% analyzeNetwork(layerGraph(Models.dlnetGenerator))
+
+trOptions   = f_set_training_options_gan(pram);
+[nets info] = f_train_gan(X.Train,X.Val,Models,trOptions);
+save(['./__trainedNetworks/'  date sprintf('nets_GAN_%s_%s_Ny%d_Nx%d_Nt%d_comp%dx.mat',pram.pattern_typ,pram.dataset,pram.Ny,pram.Nx,pram.Nt,pram.compression_fwd)],...
+      'nets','info','pram')
+dlnet_gen   = nets.dlnetGenerator;  
+
+XhatTest    = predict(dlnet_gen,gpuArray(dlarray(rand(1,1,pram.Ncompressed_gen,100,'single'),'SSCB')));
+figure;imagesc(imtile([XhatTest.extractdata]))
 
 %% image using the fwd model
 pram        = pram_init();
@@ -28,14 +47,14 @@ pram.sd_rd  = 0;
 pram.binR   = floor(sqrt(pram.compression_fwd*pram.Nt));
 
 dlnet_fwd   = f_gen_fwd(pram);  % Noise module was commented 
-dlXTest     = dlarray(X.Test(:,:,:,randi(size(X.Test,4),1,2)),'SSCB');
+%dlXTest     = dlarray(X.Test(:,:,:,randi(size(X.Test,4),1,2)),'SSCB');
 
 Yhat  = predict(dlnet_fwd,dlXTest);
 Yhat  = Yhat.extractdata * pram.amp;
 Yhat  = dlarray(poissrnd(Yhat)/pram.amp,'SSCB');% add poisson noise here seperately 
 
 figure;
-subplot(1,2,1);imagesc(X.Test(:,:,1,1));axis image
+subplot(1,2,1);imagesc(dlXTest(:,:,1,1).extractdata);axis image
 subplot(1,2,2);imagesc(Yhat(:,:,1,1).extractdata);axis image
 
 %% reconstruct with gen prior 
@@ -43,11 +62,13 @@ subplot(1,2,2);imagesc(Yhat(:,:,1,1).extractdata);axis image
 [Xhat_noPrior ,opt_info] = f_rec_noPrior(pram,dlnet_fwd,Yhat,dlXTest);
 
 figure;
-subplot(1,3,1);imagesc(dlXTest(:,:,1,1).extractdata);       axis image; axis off
-subplot(1,3,2);imagesc(Xhat_genPrior(:,:,1,1).extractdata); axis image; axis off
-subplot(1,3,3);imagesc(Xhat_noPrior(:,:,1,1).extractdata) ; axis image; axis off
+subplot(1,3,1);imagesc(dlXTest(:,:,1,2).extractdata);       axis image; axis off
+subplot(1,3,2);imagesc(Xhat_genPrior(:,:,1,2).extractdata); axis image; axis off
+subplot(1,3,3);imagesc(Xhat_noPrior(:,:,1,2).extractdata) ; axis image; axis off
 
 savepath      = ['./__results/' date '/'];
 mkdir(savepath)
-fileNameStem  = sprintf('prior_vs_noPrior_1_DMDSim_Ny%d_Nx%d_Nt%d_comp%dx',pram.Ny,pram.Nx,pram.Nt,pram.compression_fwd)
+fileNameStem  = sprintf('prior_vs_noPrior_noised_1e3_DMDSim_Ny%d_Nx%d_Nt%d_comp%dx',pram.Ny,pram.Nx,pram.Nt,pram.compression_fwd)
 saveas(gcf,[savepath fileNameStem '_fig.jpeg']);
+
+
--- pram_init.m
+++ pram_init.m
@@ -2,20 +2,20 @@
 function pram = pram_init()
 
   %% names
-  pram.mic_typ      = 'DMD';          % {'DMD','WGD'}
-  pram.pattern_typ  = 'dmd_sim_rnd';  % {'dmd_sim_rnd','dmd_exp','wgd_sim','wgd_exp'}
-  pram.dataset      = 'minist';       % {'minist','cells_dapi','cells_h2ax'}
-  pram.psf_typ      = 'gaussian';     % {'gaussian',...}
+  pram.mic_typ      = 'DMD';                          % {'DMD','WGD'}
+  pram.pattern_typ  = 'dmd_sim_rnd';                  % {'dmd_sim_rnd','dmd_exp','wgd_sim','wgd_exp'}
+  pram.dataset      = 'andrewCells_dapi_20x_maxProj'; % {'minist','andrewCells_dapi_20x_maxProj','cells_h2ax'}
+  pram.psf_typ      = 'gaussian';                     % {'gaussian',...}
   
   %% data size parameters
-  pram.Nx      = 64;
-  pram.Ny      = 64;
+  pram.Nx      = 32;
+  pram.Ny      = 32;
   pram.Nc      = 1;
   pram.Nt      = 16;
 
   %% compression parameters
   pram.compression_gen = 32;
-  pram.compression_fwd = 16;
+  pram.compression_fwd = 4;
   
   pram.Ncompressed_gen = pram.Nx*pram.Ny/pram.compression_gen;            % dim non-linear feature space
   pram.Ncompressed_fwd = pram.Nx*pram.Ny/(pram.Nt*pram.compression_fwd);  % dim measurement space
@@ -31,8 +31,8 @@ function pram = pram_init()
   pram.scale       = 0.01;                           % pramater of the leakyReLu
 
   %% training parameters
-  pram.maxEpochs          = 100;
-  pram.miniBatchSize      = 256;
+  pram.maxEpochs          = 200;
+  pram.miniBatchSize      = 512;
   pram.initLearningRate   = 1;
   pram.learningRateFactor = .1;
   pram.dropPeriod         = round(pram.maxEpochs/4);
