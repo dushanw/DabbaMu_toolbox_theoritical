524a29b0d414ea20fefedd012eebc79911989e42
Dushan N. Wadduwage <15632687+dushanw@users.noreply.github.com>
2020-09-17 05:42:47 -0700

    Minor changes to main_GANTest.

--- _x_testCodes/main_testGAN_20200911.m
+++ _x_testCodes/main_testGAN_20200911.m
@@ -10,14 +10,20 @@ pram      = pram_init();
 X         = f_get_dataset(pram);
 trOptions = f_set_training_options_gan(pram);
 
+%Models.dlnetGenerator      = f_gen_ganDeepGen(pram);
 Models.dlnetGenerator      = f_gen_ganGen(pram);
 Models.dlnetDiscriminator  = f_gen_stdDisc(pram);
-% analyzeNetwork(layerGraph(dlnetDiscriminator))
+% analyzeNetwork(layerGraph(Models.dlnetGenerator))
 
+% train new
 [nets info] = f_train_gan(X.Train,X.Val,Models,trOptions);
 dlnet_gen   = nets.dlnetGenerator;  
 
+% retrain as need be
+[nets info] = f_train_gan(X.Train,X.Val,nets,trOptions);
+dlnet_gen   = nets.dlnetGenerator;  
+
 XhatTest    = predict(dlnet_gen,gpuArray(dlarray(rand(1,1,pram.Ncompressed_gen,100,'single'),'SSCB')));
-figure;imagesc(imtile([XhatTest.extractdata]))
+figure;imagesc(imtile([XhatTest.extractdata]));axis image
 
 
